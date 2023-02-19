931b8e5c6c76613367611385a222d8995b7937a3
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-02-19 12:16:09 -0300

    Code Updated

--- plot_dataset.py
+++ plot_dataset.py
@@ -6,15 +6,13 @@ from CONSTANTS import *
 from argparse import ArgumentParser
 
 def generate_colors_for_cluster_ids(max_cluster_id):
-    color_list = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'olive', 'cyan', 'black']
+    color_list = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'olive', 'cyan', 'black', 'magenta', 'navy', 'lime']
     
     id_to_color = {}
     id_to_color = {0: 'grey'}
 
     for cluster_id in range(1, max_cluster_id+1):
         id_to_color[cluster_id] = color_list[cluster_id % len(color_list)]
-    
-    print(id_to_color)
 
     return id_to_color
 
@@ -22,6 +20,7 @@ parser = ArgumentParser()
 parser.add_argument("-f", "--file", dest="filename")
 parser.add_argument("-d", "--dimension", default='3d', dest="dimension")
 parser.add_argument("-c", "--with-clustering", default=False, dest="with_clustering")
+parser.add_argument("-s", "--save_plots", default=False, dest="save_plots")
 
 args = parser.parse_args()
 
@@ -35,7 +34,7 @@ with_clustering = args.with_clustering
 if with_clustering:
     dataset[CLUSTER_ID_COLUMN_NAME] = dataset[CLUSTER_ID_COLUMN_NAME].map(generate_colors_for_cluster_ids(max(dataset[CLUSTER_ID_COLUMN_NAME])))
 
-if projection == '3d':
+if projection == '3d' or args.save_plots:
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
@@ -48,9 +47,12 @@ if projection == '3d':
     ax.set_ylabel('t[s]')
     ax.set_zlabel('y[um]')
 
-    plt.show()
+    if args.save_plots:
+        plt.savefig(f"{args.filename}.jpg")
+    else:
+        plt.show()
 
-if projection == '2d':
+if projection == '2d' or args.save_plots:
     fig = plt.figure()
     ax = fig.add_subplot()
 
@@ -62,4 +64,7 @@ if projection == '2d':
     ax.set_xlabel('x[um]')
     ax.set_ylabel('y[um]')
 
-    plt.show()
+    if args.save_plots:
+        plt.savefig(f"{args.filename}_3d.jpg")
+    else:
+        plt.show()
