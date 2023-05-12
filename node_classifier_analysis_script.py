165488bb9f5e30b5465eac1114ac5e33901cdaa6
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-05-12 16:06:21 -0300

    Code Updated

--- node_classifier_analysis_script.py
+++ node_classifier_analysis_script.py
@@ -5,13 +5,14 @@ DATASET_PATH = 'datasets_shuffled'
 
 
 for partition_size in LocalizationClassifier.analysis_hyperparameters()['partition_size']:
-    print(f"Hyperparameter search with partition size {partition_size}")
+    print("Session Cleared...")
     K.clear_session()
+    print(f"Hyperparameter search with partition size {partition_size}")
 
     classifier = LocalizationClassifier(10,10)
 
     classifier.hyperparameters['partition_size'] = partition_size
 
-    classifier.fit_with_datasets_from_path(f'./{DATASET_PATH}/train')
+    classifier.fit_with_datasets_from_path(f'./{DATASET_PATH}/train', save_checkpoints=True)
     classifier.save_model()
-    classifier.test_with_datasets_from_path(f'./{DATASET_PATH}/test', apply_threshold=True, save_result=True, save_predictions=True, check_if_predictions_file_name_exists=False)
+    classifier.test_with_datasets_from_path(f'./{DATASET_PATH}/test', apply_threshold=True, save_result=True, save_predictions=True, check_if_predictions_file_name_exists=True)
