8bef05bad0e0bfbb8b0ba64b638dc1960defdb8a
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-06-15 23:30:19 -0300

    Code Updated

--- datasets_analysis.py
+++ datasets_analysis.py
@@ -11,11 +11,10 @@ localization_classifier.load_model()
 edge_classifier = ClusterEdgeRemover(10,10)
 edge_classifier.load_model()
 
-#TEST_DATASETS_PATH = "D:/UCA/03-Clustering Dynamics/Biological Dataset Preparation"
-TEST_DATASETS_PATH = "./datasets_shuffled/test"
+TEST_DATASETS_PATH = "D:/UCA/03-Clustering Dynamics/Biological Dataset Preparation"
 
 for dataset_file_path in [os.path.join(TEST_DATASETS_PATH, file) for file in os.listdir(TEST_DATASETS_PATH) if file.endswith('_smlm_dataset.csv')]:
     print("Predicting for:", dataset_file_path)
     smlm_dataset = pd.read_csv(dataset_file_path)
     smlm_dataset = predict_on_dataset(smlm_dataset, localization_classifier, edge_classifier)
-    smlm_dataset.to_csv(dataset_file_path+"/../.full_prediction.csv", index=False)
+    smlm_dataset.to_csv(dataset_file_path+".full_prediction.csv", index=False)
