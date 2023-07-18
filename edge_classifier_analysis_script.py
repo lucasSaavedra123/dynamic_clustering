f8fb07881ff624987ab48e9c85df99828b396160
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-07-17 22:56:15 -0300

    Code Updated

--- edge_classifier_analysis_script.py
+++ edge_classifier_analysis_script.py
@@ -20,8 +20,8 @@ for partition_size in ClusterEdgeRemover.analysis_hyperparameters()['partition_s
     classifier = ClusterEdgeRemover(10,10)
     classifier.hyperparameters['partition_size'] = partition_size
     classifier.load_model()
-
+    """
     if partition_size == 4000:
         classifier.test_with_datasets_from_path(f'./{DATASET_PATH}/test', save_result=False, detect_clusters=True, save_predictions=True, apply_threshold=True, check_if_predictions_file_name_exists=False, ignore_non_clustered_localizations=True)
-    
-    classifier.test_with_datasets_from_path(f'./{DATASET_PATH}/test', save_result=True, detect_clusters=False, save_predictions=False, apply_threshold=True, check_if_predictions_file_name_exists=True, ignore_non_clustered_localizations=False)
+    """
+    classifier.test_with_datasets_from_path(f'./{DATASET_PATH}/test', save_result=True, detect_clusters=False, save_predictions=False, apply_threshold=True, check_if_predictions_file_name_exists=True, ignore_non_clustered_localizations=True)
