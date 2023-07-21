524dba8c88c95ff85b094ebdb1b1c6729f21bd55
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-07-21 15:49:58 -0300

    tqdm added

--- storm_datasets_analysis.py
+++ storm_datasets_analysis.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from utils import predict_on_dataset
+import tqdm
 
 from LocalizationClassifier import LocalizationClassifier
 from ClusterEdgeRemover import ClusterEdgeRemover
@@ -11,10 +12,10 @@ localization_classifier.load_model()
 edge_classifier = ClusterEdgeRemover(10,10)
 edge_classifier.load_model()
 
-TEST_DATASETS_PATH = "D:/UCA/03-Clustering Dynamics/Biological Dataset Preparation"
+#TEST_DATASETS_PATH = "D:/UCA/03-Clustering Dynamics/Biological Dataset Preparation"
+TEST_DATASETS_PATH = "./Biological Dataset Preparation"
 
-for dataset_file_path in [os.path.join(TEST_DATASETS_PATH, file) for file in os.listdir(TEST_DATASETS_PATH) if file.endswith('_smlm_dataset.csv')]:
-    print("Predicting for:", dataset_file_path)
+for dataset_file_path in tqdm.tqdm([os.path.join(TEST_DATASETS_PATH, file) for file in os.listdir(TEST_DATASETS_PATH) if file.endswith('_smlm_dataset.csv')]):
     smlm_dataset = pd.read_csv(dataset_file_path)
     smlm_dataset = predict_on_dataset(smlm_dataset, localization_classifier, edge_classifier)
     smlm_dataset.to_csv(dataset_file_path+".full_prediction.csv", index=False)
--- tirf_datasets_analysis.py
+++ tirf_datasets_analysis.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from utils import predict_on_dataset
+import tqdm
 
 from CONSTANTS import *
 from LocalizationClassifier import LocalizationClassifier
@@ -12,12 +13,10 @@ localization_classifier.load_model()
 edge_classifier = ClusterEdgeRemover(None,None)
 edge_classifier.load_model()
 
-#TEST_DATASETS_PATH = "D:/UCA/03-Clustering Dynamics/Biological Dataset Preparation"
+#TEST_DATASETS_PATH = "D:/UCA/03-Clustering Dynamics/SPT-data_TIRF_DATASETS"
 TEST_DATASETS_PATH = "./SPT-data_TIRF_DATASETS"
 
-for dataset_file_path in [os.path.join(TEST_DATASETS_PATH, file) for file in os.listdir(TEST_DATASETS_PATH) if file.endswith('_tirf.csv')]:
-    print("Predicting for:", dataset_file_path)
-
+for dataset_file_path in tqdm.tqdm([os.path.join(TEST_DATASETS_PATH, file) for file in os.listdir(TEST_DATASETS_PATH) if file.endswith('_tirf.csv')]):
     smlm_dataset = pd.read_csv(dataset_file_path)
 
     new_width = smlm_dataset[X_POSITION_COLUMN_NAME].max()
