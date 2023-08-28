6693289f9a61bf6f6573012905ed9da7178aa9a4
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-08-28 19:45:43 -0300

    model classes adapted to gui

--- gui.py
+++ gui.py
@@ -4,15 +4,13 @@ This is the GUI script for Dynamic Clustering with GNNs.
 It is a code similar to https://github.com/tristanwallis/smlm_clustering/blob/main/nastic_gui.py
 but rather simple.
 """
-#MODULE IMPORTS
 import PySimpleGUI as sg
-import os
 import colorama
+import os
 
 #CONSTANTS
 METHOD_NAME = 'Dynamic Clustering with GNNs'
 LAST_UPDATE = '2023-08-28'
-
 
 if __name__ == '__main__':
     sg.theme('DARKGREY11')
@@ -21,6 +19,18 @@ if __name__ == '__main__':
     print(f'{colorama.Fore.GREEN}=================================================={colorama.Style.RESET_ALL}')
     print(f'{colorama.Fore.GREEN}{METHOD_NAME} {LAST_UPDATE} initialising...{colorama.Style.RESET_ALL}')
     print(f'{colorama.Fore.GREEN}=================================================={colorama.Style.RESET_ALL}')
+
+    #MODULE IMPORTS
+    from time import sleep
+
+    from tensorflow.errors import NotFoundError
+
+    from ClusterDetector import ClusterDetector
+    from LocalizationClassifier import LocalizationClassifier
+
+    #CONSTANTS
+    METHOD_NAME = 'Dynamic Clustering with GNNs'
+    LAST_UPDATE = '2023-08-28'
 
     layout = [
         [
@@ -53,10 +63,10 @@ if __name__ == '__main__':
         ],
         [sg.ProgressBar(100, orientation='h', expand_x=True, size=(20, 20),  key='-PBAR-')],
         [sg.Button('Analyze'), sg.Exit()],
-        [sg.Text('Current Status:', font=('Arial', 9)), sg.Text('Click on "Analyze" to start...', key='-STATUS-', font=('Arial', 9))]
+        [sg.Text('Current Status:', font=('Arial', 9)), sg.StatusBar('Click on "Analyze" to start...', key='-STATUS-', font=('Arial', 9))]
     ]
 
-    window = sg.Window('Form', layout)
+    window = sg.Window(f"{METHOD_NAME} (Last Update: {LAST_UPDATE})", layout)
 
     i = 0
 
@@ -67,9 +77,41 @@ if __name__ == '__main__':
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
         elif event == 'Analyze':
+            window['-STATUS-'].update('Loading GNN1 files...')
+            localization_classifier = LocalizationClassifier(10,10)
+
             try:
-               pass
+                localization_classifier.load_keras_model(values["-GNN1-MODEL-"], raise_error=True)
+            except NotFoundError as exception:
+                window['-STATUS-'].update('Something failed...')
+                sg.popup_error(f'Please, select a model file for GNN1.')
+                continue
+            except OSError as exception:
+                window['-STATUS-'].update('Something failed...')
+                sg.popup_error(f'Please, select a valid .h5 file for GNN1.')
+                continue
+
+            try:
+                localization_classifier.load_threshold(values["-GNN1-THRESHOLD-"], raise_error=True)
             except Exception as e:
-                sg.popup_error_with_traceback(f'An error happened. Here is the info:', e)
+                window['-STATUS-'].update('Something failed...')
+                sg.popup_error(e)
+                continue
+
+            window['-STATUS-'].update('Loading GNN2 files...')
+            cluster_detector = ClusterDetector(10,10)
+
+            try:
+                cluster_detector.load_keras_model(values["-GNN2-MODEL-"], raise_error=True)
+            except NotFoundError as exception:
+                window['-STATUS-'].update('Something failed...')
+                sg.popup_error(f'Please, select a model file for GNN2.')
+                continue
+            except OSError as exception:
+                window['-STATUS-'].update('Something failed...')
+                sg.popup_error(f'Please, select a valid .h5 file for GNN2.')
+                continue
+
+            window['-STATUS-'].update('Loading localization dataset...')
 
     window.close()
