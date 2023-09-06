ca0dfa12395d08461ab6f84f21ca8ce344d0bc7d
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-09-06 18:35:44 -0300

    .gui updated

--- dynamic_clustering/gui.py
+++ dynamic_clustering/gui.py
@@ -31,6 +31,9 @@ def start():
 
     from .algorithm.ClusterDetector import ClusterDetector
     from .algorithm.LocalizationClassifier import LocalizationClassifier
+    from .utils import predict_on_dataset
+    
+    sg.Print('Debug window open...', text_color='white', background_color='green', font='Courier 10')
 
     layout = [
         [
@@ -74,9 +77,12 @@ def start():
         sg.FileBrowse(),
         sg.Checkbox('Is a TRXYT file?', False, key='-TRXYT-FILE-OPTION-')
         ],
-        [sg.ProgressBar(100, orientation='h', expand_x=True, size=(20, 20),  key='-PBAR-')],
+        [
+        sg.Text("Path to save analyzed dataset"),
+        sg.In(size=(25, 1), enable_events=False, key="-PATH-TO-SAVE-RESULTS-"),
+        sg.FolderBrowse(),
+        ],
         [sg.Button('Analyze'), sg.Exit()],
-        [sg.Text('Current Status:', font=('Arial', 9)), sg.StatusBar('Click on "Analyze" to start...', key='-STATUS-', font=('Arial', 9))]
     ]
 
     window = sg.Window(f"{CONSTANTS.METHOD_NAME} (Last Update: {CONSTANTS.LAST_UPDATE})", layout)
@@ -90,7 +96,7 @@ def start():
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
         elif event == 'Analyze':
-            window['-STATUS-'].update('Checking ROI Dimensions...')
+            sg.Print('Checking ROI Dimensions...', text_color='white', background_color='blue', font='Courier 10')
 
             try:
                 roi_height = float(values['-HEIGHT-'])
@@ -99,38 +105,35 @@ def start():
                 if roi_height == 0 or roi_width == 0:
                     raise Exception()
             except Exception as e:
-                window['-STATUS-'].update('Something failed...')
-                sg.popup_error(f'Please, select correct ROI dimensions.')   
+                sg.Print('Please, select correct ROI dimensions.', text_color='white', background_color='red', font='Courier 10')
                 continue
-
-            window['-STATUS-'].update('Loading GNN1 files...')
+            
+            sg.Print('Loading GNN1 files...', text_color='white', background_color='blue', font='Courier 10')
             localization_classifier = LocalizationClassifier(roi_height, roi_width)
 
             try:
                 localization_classifier.load_keras_model(values['-GNN1-MODEL-'], raise_error=True)
             except Exception as e:
-                window['-STATUS-'].update('Something failed...')
-                sg.popup_error(f'Please, select a valid .h5 file for GNN1.')
+                sg.Print(f'Please, select a valid .h5 file for GNN1.', text_color='white', background_color='red', font='Courier 10')
                 continue        
 
             try:
                 localization_classifier.load_threshold(values['-GNN1-THRESHOLD-'], raise_error=True)
             except Exception as e:
-                window['-STATUS-'].update('Something failed...')
-                sg.popup_error(e)
+                sg.Print(f'Please, select a valid threshold file for GNN1.', text_color='white', background_color='red', font='Courier 10')
                 continue
 
-            window['-STATUS-'].update('Loading GNN2 files...')
+            sg.Print('Loading GNN2 files...', text_color='white', background_color='blue', font='Courier 10')
             cluster_detector = ClusterDetector(roi_height, roi_width)
 
             try:
                 cluster_detector.load_keras_model(values['-GNN2-MODEL-'], raise_error=True)
             except Exception as e:
-                window['-STATUS-'].update('Something failed...')
-                sg.popup_error(f'Please, select a valid .h5 file for GNN2.')
+                sg.Print(f'Please, select a valid .h5 file for GNN2.', text_color='white', background_color='red', font='Courier 10')
                 continue
 
-            window['-STATUS-'].update('Loading localization dataset...')
+            sg.Print('Loading localization dataset...', text_color='white', background_color='blue', font='Courier 10')
+
             try:
                 if values['-TRXYT-FILE-OPTION-']:
                     dataset = pd.read_csv(values['-LOCALIZATION-DATASET-FILE-'], sep=' ', header=None)
@@ -143,7 +146,22 @@ def start():
                 else:
                     dataset = pd.read_csv(values['-LOCALIZATION-DATASET-FILE-'])
             except Exception as e:
-                window['-STATUS-'].update('Something failed...')
-                sg.popup_error(f'Please, select a valid dataset. If a .trxyt file is not used, a .csv dataset with at least columns [x,y,t] is expected.')                
+                sg.Print(f'Please, select a valid dataset. If a .trxyt file is not used, a .csv dataset with at least columns [x,y,t] is expected.', text_color='white', background_color='red', font='Courier 10')
+                continue
+
+            if not os.path.isdir(values['-PATH-TO-SAVE-RESULTS-']):
+                sg.Print(f'Directory to save results does not exist...', text_color='white', background_color='red', font='Courier 10')
+                continue
+            
+            sg.Print(f'All is OK', text_color='white', background_color='green', font='Courier 10')
+            sg.Print(f'Analyzing...', text_color='white', background_color='green', font='Courier 10')
+
+            try:
+                dataset = predict_on_dataset(dataset, localization_classifier, dataset)
+                new_file_name = os.path.basename(values['-LOCALIZATION-DATASET-FILE-']).split('/')[-1]+".full_prediction.csv"
+                dataset.to_csv(os.path.join(values['-PATH-TO-SAVE-RESULTS-'], new_file_name), index=False)
+                sg.Print(f'FINISHED...', text_color='white', background_color='green', font='Courier 10')
+            except Exception as e:
+                sg.popup_error(e)
 
     window.close()
